0b1010bac095893858236bd093b6acd3fdf50747
LalitAswal <Lalit.Aswal143@gmail.com>
2023-08-17 21:29:14 +0530

    added comment/likes/follow field in mongodb model

--- models/postModel.js
+++ models/postModel.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const postSchema  = new Schema({
-    title:String,
-    content:String,
-    createAt:{type:Date, default:Date.now()},
-    user_id:{type: mongoose.Schema.Types.ObjectId, ref:'UserModels' }
-})
+const commentSchema = new Schema({
+    text: String,
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'UserModels' }
+});
 
+const postSchema = new Schema({
+    title: String,
+    content: String,
+    createdAt: { type: Date, default: Date.now() },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'UserModels' },
+    comments: [commentSchema], // Array of comments
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserModels' }] // Array of user IDs who liked the post
+});
 
 const postModel = mongoose.model('post', postSchema);
 
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId:String,
-    displayName:String,
-    password:String,
-    image:String,
-    firstName:String,
-    lastName:String,
-    gender:String,
-    email:String,
-})
+    googleId: String,
+    displayName: String,
+    password: String,
+    image: String,
+    firstName: String,
+    lastName: String,
+    gender: String,
+    email: String,
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserModels' }], // Array of user IDs being followed
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserModels' }] // Array of user IDs following this user
+});
 
 const UserModels = mongoose.model('UserModels', userSchema);
 
-module.exports= UserModels;
+module.exports = UserModels;
