aee203ba047a48c74fe551cfd195fcf80a1a67e6
LalitAswal <Lalit.Aswal143@gmail.com>
2023-08-19 14:05:11 +0530

    added encrypt/decrypt functionality in password

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,19 +1,25 @@
 const usersModel = require("../models/usersModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 const registrations = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+    console.log(req.body);
 
     if (!firstName || !lastName || !email || !password) {
       return res.status(400).json({ message: `Empty Fields are not allowed` });
     }
-    await usersModel.insertMany({
+
+    const hashedPassword = await bcrypt.hash(password, 10); // Using 10 salt rounds
+
+    await usersModel.create({
       firstName: firstName,
       lastName: lastName,
       email: email,
-      password: password,
+      password: hashedPassword,
     });
+    console.log(`checking registration`);
 
     return res.status(200).json({ message: `Successfully updated` });
   } catch (error) {
@@ -29,9 +35,11 @@ const login = async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ message: `Empty Fields are not allowed` });
     }
-    let result = await usersModel.findOne({ email: email, password: password });
-    console.log(result);
-    if (!result) {
+    let result = await usersModel.findOne({ email: email});
+    
+    let passwordCheck = bcrypt.compare(password, result.password);
+
+    if (!passwordCheck) {
       return res.status(400).json({ message: `Invalid Email or Password` });
     }
     const expireTime = process.env.jwtExpiryTime;
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,35 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const crypto = require('crypto');
-
-
-let key = process.env.key;
-let iv = process.env.iv;
-
-function encrypt(text) {
-    const cipher = crypto.createCipheriv(process.env.cryptoAlgo, key, iv);
-    let encrypted = cipher.update(text, "utf8", "hex");
-    encrypted += cipher.final("hex");
-    return encrypted;
-  }
-  
-  function decrypt(text) {
-    const decipher = crypto.createDecipheriv(
-      process.env.cryptoAlgo,
-      key,
-      iv
-    );
-    let decrypted = decipher.update(text, "hex", "utf8");
-    decrypted += decipher.final("utf8");
-    return decrypted;
-  }
-
 
 
 const userSchema = new Schema({
     googleId: String,
     displayName: String,
-    password: {type: String, get: decrypt, set: encrypt },
+    password:  String,
     image: String,
     firstName: String,
     lastName: String,
